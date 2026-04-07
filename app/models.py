9ad145173eeb410d1211ab0af338de383a3f69a0
maffiix <kirilchikmihail4@gmail.com>
2026-04-07 15:34:45 +0300

    GOT FIRST CHECKING TASK 🎉

--- app/models.py
+++ app/models.py
@@ -16,3 +16,27 @@ class User(db.Model, UserMixin):
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
+
+
+class Level(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    title = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+
+    starter_code = db.Column(db.Text)
+
+    order = db.Column(db.Integer, unique=True)
+    checker_name = db.Column(db.String(50), nullable=False)
+
+
+class UserProgress(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    level_id = db.Column(db.Integer, db.ForeignKey("level.id"))
+
+    completed = db.Column(db.Boolean, default=False)
+
+    user = db.relationship("User", backref="progress")
+    level = db.relationship("Level")
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, redirect, render_template, request, url_for
-from flask_login import login_user, login_required
-from .models import User
+from flask_login import login_user, login_required, current_user, logout_user
+from .models import User, Level, UserProgress
+from app.checker.runner import run_checker
 from app import db, login_manager
 
 main = Blueprint("main", __name__)
@@ -9,6 +10,13 @@ main = Blueprint("main", __name__)
 @main.route("/")
 def index():
     return render_template("index.html")
+
+
+@main.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.login"))
 
 
 @main.route("/register", methods=["GET", "POST"])
@@ -32,6 +40,31 @@ def register():
     return redirect(url_for("main.login"))
 
 
+@main.route("/level/<int:level_id>", method="GET")
+@login_required
+def level(level_id):
+    level = Level.query.get_or_404(level_id)
+    return render_template("level.html", level=level)
+
+
+@main.route("/levels")
+@login_required
+def levels():
+
+    levels = Level.query.order_by(Level.order).all()
+
+    completed_ids = {
+        p.level_id
+        for p in UserProgress.query.filter_by(user_id=current_user.id, completed=True)
+    }
+
+    return render_template(
+        "levels.html",
+        levels=levels,
+        completed_ids=completed_ids
+    )
+
+
 @main.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "GET":
@@ -44,15 +77,42 @@ def login():
 
     if user and user.check_password(password):
         login_user(user)
-        return redirect(url_for("main.dashboard"))
+        return redirect(url_for("main.levels"))
 
     return render_template("login.html", message="Invalid username or password")
 
 
+@main.route("/level/<int:level_id>", methods=["POST"])
 @login_required
-@main.route("/dashboard")
-def dashboard():
-    return "SECRET PAGE NAHH"
+def submit_solution(level_id):
+
+    level = Level.query.get_or_404(level_id)
+
+    code = request.form["code"]
+
+    success, message = run_checker(level.checker_name, code)
+
+    if success:
+        progress = UserProgress.query.filter_by(
+            user_id=current_user.id,
+            level_id=level.id
+        ).first()
+
+        if not progress:
+            progress = UserProgress()
+            progress.user_id = current_user.id
+            progress.level_id = level.id
+            progress.completed = True
+            db.session.add(progress)
+            db.session.commit()
+
+        return redirect(url_for("main.levels"))
+
+    return render_template(
+        "level.html",
+        level=level,
+        error=message
+    )
 
 
 @login_manager.user_loader
