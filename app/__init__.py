754dac8facf296beabad3bc8b5450a0241bc5b4d
maffiix <kirilchikmihail4@gmail.com>
2026-04-17 20:19:31 +0300

    commit also

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 login_manager = LoginManager()
 
 
-def nnncreate_app():
+def create_app():
     app = Flask(__name__)
     app.config.from_object("config.Config")
     db.init_app(app)
