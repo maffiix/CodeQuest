ad41727850e327daf77973f23022e784c218491c
007maximilian <max.mazu007@gmail.com>
2026-04-28 18:41:18 +0300

    Тесты для 2 задачи

--- app/checker/levels/level_2.py
+++ app/checker/levels/level_2.py
@@ -3,11 +3,38 @@ import subprocess
 
 def check(user_code: str):
     tests = [
-        ("1\n2\n", "3"),
-        ("o\n0\n", "0"),
-        ("1\n1\n", "2"),
-        ("6\n6\n", "12"),
-        ("100\n100\n", "200")
+        ("""BH-01-1
+BH-01-2
+END
+J-1212 J-24:10 J-50:5 J-60:7 BH-01-1:1
+J-24 J-1212:10 J-70:4 J-80:6 BH-01-2:1
+J-50 J-1212:5 J-90:3 J-100:2 J-110:4
+J-60 J-1212:7 J-120:5 J-130:6 J-140:8
+END
+J-50:J-90
+END_ALL""", "BH-01-1 41"),
+        ("""BH-1-1
+BH-01-2
+END
+J-1212 J-24:10 J-50:5 J-60:7 BH-01-2:1
+J-24 J-1212:10 J-70:4 J-80:6 J-90:3
+END
+END_ALL""", "INCORRECT INPUT: invalid exit format 'BH-1-1'"),
+        ("""BH-01-1
+END
+J-1212 J-24:10 J-50:5 BH-01-1:1
+J-24 J-1212:10 J-70:4 J-80:6 J-90:3
+END
+END_ALL""", "INCORRECT INPUT: node J-1212 has 3 connections, expected 4"),
+        ("""BH-01-1
+BH-01-2
+END
+J-1212 J-50:5 J-60:7 J-70:3 BH-01-1:1
+J-24 J-80:4 J-90:2 J-100:6 BH-01-2:1
+J-50 J-1212:5 J-110:4 J-120:3 J-130:2
+J-80 J-24:4 J-140:5 J-150:6 J-160:7
+END
+END_ALL""", "INCORRECT INPUT: no valid path found")
     ]
 
     for i, (inp, expected) in enumerate(tests, start=1):
